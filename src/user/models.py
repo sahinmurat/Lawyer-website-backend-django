1587ad9e1e584edcea2b7ba87b8bfdb7f5fcfae1
Murat Sahin <yazilimogren1111@gmail.com>
2021-04-12 17:34:38 +0200

    user registration added, user models and serializer created

--- src/user/models.py
+++ src/user/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+def user_profile_path(instance, filename):
+    return 'user/{0}/{1}'.format(instance.user.id, filename)
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    email = models.EmailField(max_length=100)
+    image = models.URLField(
+        max_length=2000, default="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTayBxqNcpOECgVloLid0g8WYj7qn6w7k-dhQ&usqp=CAU")
+    bio = models.TextField(max_length = 500 , blank=True)
+
+    def __str__(self):
+        return "{} {}".format(self.user, 'Profile')
